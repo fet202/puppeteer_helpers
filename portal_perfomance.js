07a7c7d2ce7f4de6cabc065c0e25750994d45b8b
fet202 <31649268+fet202@users.noreply.github.com>
2019-11-17 17:35:29 +0300

    Create portal_perfomance.js

---
+++ portal_perfomance.js
@@ -0,0 +1,17 @@
+
+const puppeteer = require('puppeteer');
+
+(async () => {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+
+  await page.goto('https://pptr.dev');
+  
+  // Executes Navigation API within the page context
+  const metrics = await page.evaluate(() => JSON.stringify(window.performance));
+
+  // Parses the result to JSON
+  console.info(JSON.parse(metrics));
+
+  await browser.close();
+})();
